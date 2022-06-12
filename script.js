2d4982fa83afeea3d5a3a8527f865cef59f9e163
Vaidean Cristina Mihaela <72603030+Cristina2020-art@users.noreply.github.com>
2022-06-12 10:32:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// form.js
+...
+
+/**
+ * This function gets the values in the form
+ * and returns them as an object with the
+ * [formIdentifier] as the object key
+ * @returns {Object}
+ */
+const getFormData = () => {
+  let data = { [formIdentifier]: {} }; // create an empty object with the formIdentifier as the key and an empty object as its value
+  for (const element of formElements) {
+    if (element.name.length > 0) {
+      data[formIdentifier][element.name] = element.value;
+    }
+  }
+  return data;
+};
+
+saveButton.onclick = event => {
+  event.preventDefault();
+  data = getFormData();
+  localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));
+  const message = "Form draft has been saved!";
+  displayAlert(message);
+};
+
+/**
+ * This function displays a message
+ * on the page for 1 second
+ *
+ * @param {String} message
+ */
+const displayAlert = message => {
+  alertBox.innerText = message; // add the message into the alert box
+  alertBox.style.display = "block"; // make the alert box visible
+  setTimeout(function() {
+    alertBox.style.display = "none"; // hide the alert box after 1 second
+  }, 1000);
+};
